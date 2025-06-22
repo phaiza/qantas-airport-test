040d1a3aaa8a8fb05fa04fd614f6ec6a9972bc8b
fahmed1 <fahmed1@woolworths.com.au>
2025-06-22 16:26:20 +1000

    Add AirportCard, AirportList, and HomePage with dummy data

---
+++ src/data/airports.js
@@ -0,0 +1,26 @@
+export const airports = [
+  {
+    airportCode: 'AAA',
+    airportName: 'Anaa',
+    city: { cityName: 'Anaa', timeZoneName: 'Pacific/Tahiti' },
+    country: { countryName: 'French Polynesia' },
+    region: { regionName: 'South Pacific' },
+    location: { latitude: 17.25, longitude: 145.3 },
+  },
+  {
+    airportCode: 'AAB',
+    airportName: 'Arrabury',
+    city: { cityName: 'Arrabury', timeZoneName: 'Australia/Brisbane' },
+    country: { countryName: 'Australia' },
+    region: { regionName: 'Australia' },
+    location: { latitude: 26.45, longitude: 141.0 },
+  },
+  {
+    airportCode: 'AAC',
+    airportName: 'Al Arish',
+    city: { cityName: 'Al Arish', timeZoneName: 'Africa/Cairo' },
+    country: { countryName: 'Egypt' },
+    region: { regionName: 'Africa' },
+    location: { latitude: 31.1, longitude: 33.5 },
+  },
+];
